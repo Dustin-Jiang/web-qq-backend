3cb2cdfb50242a8c0b86036e6ee8c552d078a83a
Dustin Jiang <dustinjiang@outlook.com>
2021-12-24 06:45:36 +0000

    [Content] Get login QR code

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const { createClient } = require("oicq")
+const express = require("express")
+const app = express()
+const fs = require("fs")
+
+const account = [2752805684]
+var accountList = {}
+
+for (i of account) {
+  var client = createClient(account)
+
+  client.on("system.online", () => console.log("Logged in!"))
+  client.on("message", e => {
+    console.log(e)
+    // e.reply("hello world", true) //true表示引用对方的消息
+  })
+
+  client.on("system.login.qrcode", function (e) {
+    //扫码后等待请求登录
+    app.get("/login/qrcode/:id", (req, res) => {
+      const rs = fs.createReadStream(`./src/data/${req.params.id}/qrcode.png`)
+      rs.pipe(res)
+      rs.on("error", () => res.status(404).send("User Not Found"))
+    })
+    app.get("/login/scan", (req, res) => {
+      console.log("Finish Scanning")
+      client.login()
+      client.on("internal.error.qrcode", (retcode, message) => {
+        res.status(500).send(message)
+      })
+    })
+  }).login()
+
+  accountList[i.toString()] = client
+}
+
+app.get("/", (req, res) => {
+  res.send("OICQ Backend")
+})
+
+app.listen(5000)
